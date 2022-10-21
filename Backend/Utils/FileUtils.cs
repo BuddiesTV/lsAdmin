1f3e2e5dbc3943f03ada15e210fd5ed14d6e6728
BuddiesTV <BuddiesTV@outlook.de>
2022-10-21 04:17:03 +0200

    Add FileUtils

---
+++ Backend/Utils/FileUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend.Utils
+{
+    public class FileUtils
+    {
+        public string GetFileHash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return Encoding.Default.GetString(md5.ComputeHash(stream));
+                }
+            }
+        }
+    }
+}
