b7934a60c23cc3aeb8d7d23e56d8d634bf1d3459
BuddiesTV <BuddiesTV@outlook.de>
2022-10-21 04:18:08 +0200

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Backend
+﻿using Backend.Managers;
+using Backend.Enums;
+
+namespace Backend
 {
     public class Program
     {
+        public ServerManager serverManager;
+
+        public Program()
+        {
+            serverManager = new();
+
+            serverManager.serverDownloader.DownloadServer(Branch.dev, Platform.x64_win32, new Modules[] { Modules.csharp }, "D:/ServerManager/Tests");
+        }
+
         static void Main(string[] args)
         {
-            
+            new Program();
+
+            Console.Read();
         }
     }
 }
